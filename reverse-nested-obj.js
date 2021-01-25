1a8c2530f12ce79b7fa80fead2bab2f9b53cdc3b
Kaikai8888 <wuky812@gmail.com>
2021-01-25 16:51:23 +0800

    clear question description in js file already listed in README

--- reverse-nested-obj.js
+++ reverse-nested-obj.js
@@ -1,36 +1,3 @@
-/**
- * Please use TypeScript/JS to answer question
- * Welcome to answer with unit testing code if you can
- *
- * After you finish coding, please push to your GitHub account and share the link with us.
- */
-
-// Please write a function to reverse the following nested input value into output value
-
-// Input:
-let inputValue = {
-  hired: {
-    be: {
-      to: {
-        deserve: 'I'
-      }
-    }
-  }
-};
-
-// Output:
-let outputValue = {
-  I: {
-    deserve: {
-      to: {
-        be: 'hired'
-      }
-    }
-  }
-};
-
-
-//------Answer------//
 function reverseNestedObj(inputValue) {
   if (!isObject(inputValue)) throw new Error('InputValue must be an object')
   if (!Object.keys(inputValue).length) return inputValue
@@ -61,8 +28,5 @@ function wrapObject(variable, key) {
 function isObject(variable) {
   return Object.prototype.toString.call(variable) === '[object Object]'
 }
-
-// const result = reverseNestedObj(inputValue)
-// console.log('result', result)
 
 module.exports = reverseNestedObj
